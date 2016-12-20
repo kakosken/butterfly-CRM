b2ddd924bcf1049ee315bb5d93eecdbf3babdb80
TerhiJii <jarvenpaa.terhi@gmail.com>
2016-12-20 15:44:00 +0200

    Changed Order.java

--- src/main/java/CRM/Order.java
+++ src/main/java/CRM/Order.java
@@ -1,5 +1,34 @@
 package CRM;
 
+import java.io.Serializable;
+
+import javax.persistence.*;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+@ManagedBean
+@RequestScoped
+//@Entity
 public class Order {
+//  private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private enum state{
+    	NEW, DELIVERED
+    }; 
+    private String salesPerson;
+	public Order(Long id, String salesPerson) {
+		super();
+		this.id = id;
+		this.salesPerson = salesPerson;
+	}
+    
+   /* public Order() {
+		super();
+	}*/
+    
+    
 
 }
